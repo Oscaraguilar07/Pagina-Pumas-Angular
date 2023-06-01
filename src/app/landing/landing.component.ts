49d06e3d3708e52770d528dae4b7e6b46b239f3d
Oscaraguilar07 <125697206+Oscaraguilar07@users.noreply.github.com>
2023-06-01 18:55:06 -0500

    Add files via upload

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent {
+
+}
